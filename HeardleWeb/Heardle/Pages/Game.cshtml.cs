da1f3b4f6f01c34aed7f0e1ff0943b7e472bff02
Jack McCorkindale <jdmccork@gmail.com>
2023-02-12 12:28:24 +1300

    Add timeout to pause song after set time Improved messages

--- HeardleWeb/Heardle/Pages/Game.cshtml.cs
+++ HeardleWeb/Heardle/Pages/Game.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
@@ -16,11 +17,9 @@ namespace Heardle.Pages
 		private readonly SpotifyClientBuilder _spotifyClientBuilder;
 
 		[BindProperty]
-		public string Guess { get; set; }
-		[BindProperty]
 		public FullTrack CurrentSong { get; set; }
 		[BindProperty]
-		public string Message { get; set; }
+		public string Message { get; set; } = "Loading Context...";
 		public Paging<SavedTrack> PlaylistSongInfo { get; set; }
 		public string Token { get; set; }
 
@@ -35,14 +34,16 @@ namespace Heardle.Pages
 		public async Task OnGet()
 		{
 			var spotify = await _spotifyClientBuilder.BuildClient();
-			if (HttpContext.Session.GetString("PlaylistSongInfo") != null)
+			var x = HttpContext.Session.GetString("PlaylistSongInfo");
+			if (x == null)
 			{
-				CurrentSong = JsonConvert.DeserializeObject<FullTrack>(HttpContext.Session.GetString("CurrentSong"));
-				return;
+				PlaylistSongInfo = await spotify.Library.GetTracks(new LibraryTracksRequest { Limit = 1, Offset = 0 });
+				HttpContext.Session.SetString("PlaylistSongInfo", JsonConvert.SerializeObject(PlaylistSongInfo));
+			} else
+			{
+				PlaylistSongInfo = JsonConvert.DeserializeObject<Paging<SavedTrack>>(x);
 			}
 
-			PlaylistSongInfo = await spotify.Library.GetTracks(new LibraryTracksRequest { Limit = 1, Offset = 0 });
-			HttpContext.Session.SetString("PlaylistSongInfo", JsonConvert.SerializeObject(PlaylistSongInfo));
 			Console.WriteLine(PlaylistSongInfo + " containing " + PlaylistSongInfo.Total + " tracks");
 
 			if (PlaylistSongInfo == null || PlaylistSongInfo.Total == 0)
@@ -51,11 +52,10 @@ namespace Heardle.Pages
 				return;
 			}
 
-			await GetNextSong();
 			return;
 		}
 
-		private async Task GetNextSong()
+		public async Task<PartialViewResult> OnGetNextSong()
 		{
 			var spotify = await _spotifyClientBuilder.BuildClient();
 			PlaylistSongInfo = JsonConvert.DeserializeObject<Paging<SavedTrack>>(HttpContext.Session.GetString("PlaylistSongInfo"));
@@ -66,15 +66,29 @@ namespace Heardle.Pages
 			CurrentSong = likedSongs.Items[0].Track;
 			Console.WriteLine(CurrentSong.Name);
 			HttpContext.Session.SetString("CurrentSong", JsonConvert.SerializeObject(CurrentSong));
+
+			return PartialView("songData", this);
 		}
 
-		public async Task OnPostGuess()
+		public async Task<IActionResult> OnPostPlayTrack(int timeout)
+		{
+			var spotify = await _spotifyClientBuilder.BuildClient();
+			var deviceId = (HttpContext.Session.GetString("deviceId"));
+			CurrentSong = JsonConvert.DeserializeObject<FullTrack>(HttpContext.Session.GetString("CurrentSong"));
+
+			var playRequest = new PlayerResumePlaybackRequest() { Uris = new List<string>() { CurrentSong.Uri }, DeviceId = deviceId, PositionMs = 0 };
+			await spotify.Player.ResumePlayback(playRequest);
+
+			return new JsonResult("Playing track " + CurrentSong.Name);
+		}
+
+		public async Task<IActionResult> OnPostGuess(string guess)
 		{
 			var spotify = await _spotifyClientBuilder.BuildClient();
 			CurrentSong = JsonConvert.DeserializeObject<FullTrack>(HttpContext.Session.GetString("CurrentSong"));
 
-			var guess = new SearchRequest(SearchRequest.Types.Track, Guess);
-			var guessedSong = (await spotify.Search.Item(guess)).Tracks.Items.FirstOrDefault();
+			var guessResponse = new SearchRequest(SearchRequest.Types.Track, guess);
+			var guessedSong = (await spotify.Search.Item(guessResponse)).Tracks.Items.FirstOrDefault();
 
 			Console.WriteLine("Correct Song: " + CurrentSong.Href + ", " + CurrentSong.Name + " by: " + String.Join(" & ", CurrentSong.Artists.Select(x => x.Name)));
 			Console.WriteLine("Song Guessed: " + guessedSong.Href + ", " + guessedSong.Name + " by: " + String.Join(" & ", guessedSong.Artists.Select(x => x.Name)));
@@ -82,20 +96,14 @@ namespace Heardle.Pages
 			Console.WriteLine(guessedSong.Name == CurrentSong.Name);
 			Console.WriteLine(guessedSong.Artists.Select(x => x.Name).Except(CurrentSong.Artists.Select(x => x.Name)).ToList().Count);
 
-			if (guessedSong.Id == CurrentSong.Id 
-				|| (
-					guessedSong.Name == CurrentSong.Name 
-					&& guessedSong.Artists.Select(x => x.Name).Except(CurrentSong.Artists.Select(x => x.Name)).ToList().Count == 0))
+			if (guessedSong.Id == CurrentSong.Id || (guessedSong.Name == CurrentSong.Name && guessedSong.Artists.Select(x => x.Name).Except(CurrentSong.Artists.Select(x => x.Name)).ToList().Count == 0))
 			{
-				Message = "Correct Guess";
-				Console.WriteLine("Correct guess");
-				await GetNextSong();
+				Console.WriteLine("Correct guessResponse");
+				return new JsonResult(true);
 			}
-			else
-			{
-				Message = "Incorrect Guess";
-				Console.WriteLine("Incorrect Guess");
-			}
+
+			Console.WriteLine("Incorrect Guess");
+			return new JsonResult(false);
 		}
 
 		public IActionResult OnPostAutoComplete(string search)
@@ -114,23 +122,23 @@ namespace Heardle.Pages
 			return new JsonResult(candidateSongs);
 		}
 
-		public async Task<IActionResult> OnPostConnectSDK(string deviceId)
+		public IActionResult OnPostConnectSDK(string deviceId)
 		{
-			var spotify = await _spotifyClientBuilder.BuildClient();
-			CurrentSong = JsonConvert.DeserializeObject<FullTrack>(HttpContext.Session.GetString("CurrentSong"));
-
-			var response = await spotify.Player.TransferPlayback(new PlayerTransferPlaybackRequest(new List<string>() { deviceId }));
-			var request = new PlayerResumePlaybackRequest() { Uris = new List<string>() { CurrentSong.Uri }, DeviceId = deviceId, PositionMs = 0 };
-
-			var playback = await spotify.Player.ResumePlayback(request);
-
-			return new JsonResult(response);
+			HttpContext.Session.SetString("deviceId", deviceId);
+			return new JsonResult("Player Loaded. Getting Song.");
 		}
 
-		public async Task<IActionResult> OnPost()
+		[NonAction]
+		public virtual PartialViewResult PartialView(string viewName, object model)
 		{
-			await HttpContext.SignOutAsync();
-			return Redirect("/");
+			ViewData.Model = model;
+
+			return new PartialViewResult()
+			{
+				ViewName = viewName,
+				ViewData = ViewData,
+				TempData = TempData
+			};
 		}
 	}
 
